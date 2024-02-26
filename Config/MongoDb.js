0eb64da5c83de3eb6581608b5d3775448792b6d0
SangramYadav4149 <sangramy114@gmail.com>
2024-02-27 00:24:00 +0530

    n

--- Config/MongoDb.js
+++ Config/MongoDb.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectMongoDb = async () => {
   try {
-    const connect = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    const connect = await mongoose.connect(
+      `mongodb://127.0.0.1:27017/pro-manage`
+    );
     console.log(`DATABASE CONNECTED ${connect.connection.host}`);
   } catch (error) {
     console.log(`Error :${error.message}`);
